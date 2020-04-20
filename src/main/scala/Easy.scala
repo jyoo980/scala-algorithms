dda5adf0c722f138be2de565a9a5373d747cfde8
James Yoo <jyoo980@gmail.com>
2020-04-19 18:03:43 -0700

    adding findNumber and twoSum

--- src/main/scala/Easy.scala
+++ src/main/scala/Easy.scala
@@ -4,4 +4,28 @@ object Easy {
     s.split(" ").collect {
       case seg if seg.nonEmpty => 1
     }.sum
+
+  // https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+  def findNumbers(nums: Array[Int]): Int = {
+    def evenNumDigits(n: Int): Boolean = {
+      n.toString.length % 2 == 0
+    }
+    nums.collect {
+      case n if evenNumDigits(n) => 1
+    }.sum
+  }
+
+  // https://leetcode.com/problems/two-sum/
+  def twoSum(nums: Array[Int], target: Int): Option[(Int, Int)] = {
+    val indexedNums: List[(Int, Int)] = nums.zipWithIndex.toList
+    val diffMap = indexedNums.foldLeft(Map[Int, Int]()) { (acc, tup) =>
+      tup match {
+        case (n, i) => acc + ((target - n) -> i)
+      }
+    }
+    indexedNums.collectFirst {
+      case (n, i) if diffMap.getOrElse(n, -1) != -1 =>
+        (i, diffMap.get(n).fold(-1)(j => j))
+    }
+  }
 }
--- src/test/scala/TestEasy.scala
+++ src/test/scala/TestEasy.scala
@@ -16,4 +16,38 @@ class TestEasy extends FunSpec {
       assert(Easy.countSegments("hello world") == 2)
     }
   }
+
+  describe("Easy::findNumbers") {
+
+    it("should handle an empty array") {
+      assert(Easy.findNumbers(Array()) == 0)
+    }
+
+    it("should handle arrays with numbers that all have have an odd number of digits") {
+      assert(Easy.findNumbers(Array(1, 555, 12323)) == 0)
+    }
+
+    it("should handle arrays with a few numbers that have an even number of digits") {
+      assert(Easy.findNumbers(Array(11, 3333, 5, 67546)) == 2)
+    }
+  }
+
+  describe("Easy::twoSum") {
+
+    it("should evaluate to None if there are two numbers that do not add to target") {
+      val nums = Array(5, 4, 3, 0, 23)
+      assert(Easy.twoSum(nums, -1) match {
+        case Some(_) => false
+        case None => true
+      })
+    }
+
+    it("should evaluate to Some if there are two numbers that do add to target") {
+      val nums = Array(4, 6, 11, 12, 9)
+      assert(Easy.twoSum(nums, 23) match {
+        case Some(indices) => indices == (2, 3)
+        case None => false
+      })
+    }
+  }
 }
